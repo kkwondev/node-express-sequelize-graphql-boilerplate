ae00937d2984f42f0359d0a71c15b928f42dbfe4
kkwoncokr <kkwoncokr@gmail.com>
2021-03-06 01:01:58 +0900

    [FEAT] login.
    Successfully added login logic.
    Returns to JWT token value BODY value when logging in.

--- src/resolverMap.ts
+++ src/resolverMap.ts
@@ -1,6 +1,9 @@
 import {IResolvers} from 'graphql-tools';
 import { Users } from './models/user';
 import bcrypt from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const resolverMap: IResolvers = {
     Query: {
@@ -34,8 +37,27 @@ const resolverMap: IResolvers = {
                phoneNumber:context.input.phoneNumber
            })
            return create
-            }
+            },
+        login:async(args,{input}) => {
+            const {email,password} = input;
+            const user = await Users.findOne({
+               where:{
+                   email:email
+               }
+            })
+            if(!user)  throw new Error('존재하지 않은 이메일입니다.')
+            const valid = await bcrypt.compare(password, user.password);
+            if(!valid) throw new Error('비밀번호가 일치하지 않습니다.')
+            return jsonwebtoken.sign(
+                {
+                id:user.id,
+                email:user.email,
+            },
+           process.env.JWT_SECRET as string,
+            {expiresIn:'1h'}
+            );
         }
     }
+}
 
 export default resolverMap;
